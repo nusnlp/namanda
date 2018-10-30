a2304a06f5320ff68ac4d264dca75ad8d267635f
Souvik Kundu <SOUVIK@r-98-104-25-172.comp.nus.edu.sg>
2018-10-30 15:12:21 +0800

    update

--- NAMANDA/scripts/reader/predict.py
+++ NAMANDA/scripts/reader/predict.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-# Copyright 2017-present, Facebook, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-"""A script to make and save model predictions on an input dataset."""
 
 import os
 import time
